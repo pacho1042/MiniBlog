dd1741da5784c064dad250d410db2688cafb20a5
Francisco Delgado <pacho1042@gmail.com>
2026-09-20 11:38:30 -0500

    feat(db/config.js): actuliza la conexion pool al servidor externo

--- src/db/config.js
+++ src/db/config.js
@@ -2,12 +2,15 @@ import { Pool } from "pg";
 
 //creamos el objeto para las conexiones
 const pool = new Pool({
-    
+   /* Por separado para una conexión local sin el uso del servidor externo 
    host:        process.env.DB_HOST,
    port:        process.env.DB_PORT,
    database :   process.env.DB_NAME,
    user:        process.env.DB_USER,
    password:    process.env.DB_PASSWORD
+   */
+  /*Conexión a servidor externo*/
+  connectionString:process.env.DATABASE_URL,
 
 });
 
